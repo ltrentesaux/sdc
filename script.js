e8e71d069f009711dc8743e8cd1a985374c93827
leo.trentesaux53 <leo.trentesaux53@gmail.com>
2024-10-15 07:47:02 +0000

    CSS

--- script.js
+++ script.js
@@ -16,14 +16,12 @@ function displayRecipes(recipes) {
             <img src="ressources/images/${recipe.image}" alt="Image de ${recipe.name}">
             <h3>${recipe.name}</h3>
             <p><strong>Recette:</strong></p>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut tortor porttitor, condimentum mauris ac, dignissim tellus. Integer at posuere lacus. Donec vitae ipsum elit. Integer vehicula mattis tempor. Aliquam ac congue diam. Nunc ut sollicitudin justo. Praesent mattis tellus non neque congue, non vehicula nunc facilisis. Vivamus nec arcu lectus. Praesent maximus vitae tortor sit amet finibus. Morbi efficitur orci sit amet erat ullamcorper suscipit. Aliquam aliquam, est vel aliquet maximus, mauris mauris finibus mi, ut ullamcorper metus quam nec lacus. Sed efficitur sed sem id ultricies. Vivamus in justo elit. Etiam nec malesuada quam, ut sagittis turpis. Maecenas sit amet turpis eleifend nibh vehicula consectetur eget eu magna. 
-            </p>
+            <p> ${recipe.description} </p>
             <p><strong>Ingrédients:</strong></p>
             <ul>
                 ${recipe.ingredients.map(ingredient => `
                     <li>
-                        ${ingredient.ingredient} 
+                        <b> ${ingredient.ingredient} </b> <br>
                         ${ingredient.quantity ? `: ${ingredient.quantity}` : ''} 
                         ${ingredient.unit ? ingredient.unit : ''}
                     </li>
